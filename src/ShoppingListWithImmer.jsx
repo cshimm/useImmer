9030b30dc1f5ddd79371338c5587c8cb50735b69
cshimm <cameronjshimmin@gmail.com>
2024-03-09 18:19:34 -0800

    explanations

--- src/ShoppingListWithImmer.jsx
+++ src/ShoppingListWithImmer.jsx
@@ -18,26 +18,35 @@ export const ShoppingListWithImmer = () => {
                 notes
             }
         }
+        // I use useImmer to easily update the list of shopping items with the draft argument.
         setShoppingList(draft => {
             draft.push(newItem);
         });
     }
     const updateItem = (id) => {
+        //Here I check if the id passed in matches any in the shopping list.
+        // if so we can easily update values using dot notation rather than
+        // using the spread operator on a nested object.
+        // The update values are arbitrary but from ths example we can see
+        // the power of useImmer to update complex objects.
         setShoppingList(draft => {
             draft.map(item => {
                 if (item.id === id) {
-                    item.name = 'update name'
-                    item.qty = item.qty === '' ? 0 : parseInt(item.qty) + 1
-                    item.details.category = 'updated category'
-                    item.details.notes = 'updated notes'
+                    item.name = 'update name';
+                    item.qty = item.qty === '' ? 0 : parseInt(item.qty) + 1;
+                    item.details.category = 'updated category';
+                    item.details.notes = 'updated notes';
                 }
                 return item
             });
         })
     }
     const removeItem = (id) => {
+        //Check for an existing item who's id matches the id passed in
         const existing = shoppingList.filter(item => item.id === id)[0];
         if (!existing) return
+        // If there is a matching item then save its index and splice it out of the
+        // items array.
         const existingIndex = shoppingList.indexOf(existing)
         setShoppingList(draft => {
             draft.splice(existingIndex, 1)
